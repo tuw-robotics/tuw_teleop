b177a5aaf8f2351d8918b08edb30df8510fdca3a
FiratBerkCakar <firatberkcakar@gmail.com>
2019-07-23 11:01:50 +0200

    refactor patroling

--- tuw_patroling/scripts/patroling.py
+++ tuw_patroling/scripts/patroling.py
@@ -6,77 +6,78 @@ from tuw_multi_robot_msgs.msg import RobotInfo
 from geometry_msgs.msg import PoseStamped
 
 
-def get_goal_pose_from_cfg(goal_name_identifier):
-    new_pose = PoseStamped()
-    goal_position_x = '~goal_position_x_' + goal_name_identifier
-    new_pose.pose.position.x = rospy.get_param(goal_position_x)
+class Patroling:
+    def __init__(self, number_of_goals=2):
+        self.counter = 0
+        self.goal_poses_index = 0
+        rospy.init_node('patroling', anonymous=True)
+        self.goal_poses = []
+        self.number_of_goals = number_of_goals
+        self.get_goal_poses()
+        self.next_goal_pose = self.goal_poses[self.goal_poses_index]
+        self.threshold_in_meters = 0.5
+        self.pub = rospy.Publisher('goal', PoseStamped, queue_size=10)
+        self.sub = rospy.Subscriber("robot_info", RobotInfo, self.robot_info_callback)
+        self.rate = rospy.Rate(2)  # 10hz
 
-    goal_poisition_y = '~goal_position_y_' + goal_name_identifier
-    new_pose.pose.position.y = rospy.get_param(goal_poisition_y)
+    def get_goal_pose_from_cfg(self, goal_name_identifier):
+        new_pose = PoseStamped()
+        goal_position_x = '~goal_position_x_' + goal_name_identifier
+        new_pose.pose.position.x = rospy.get_param(goal_position_x)
 
-    goal_orientation_z = '~goal_orientation_z_' + goal_name_identifier
-    new_pose.pose.orientation.z = rospy.get_param(goal_orientation_z)
+        goal_poisition_y = '~goal_position_y_' + goal_name_identifier
+        new_pose.pose.position.y = rospy.get_param(goal_poisition_y)
 
-    goal_orientation_w = '~goal_orientation_w_' + goal_name_identifier
-    new_pose.pose.orientation.w = rospy.get_param(goal_orientation_w)
+        goal_orientation_z = '~goal_orientation_z_' + goal_name_identifier
+        new_pose.pose.orientation.z = rospy.get_param(goal_orientation_z)
 
-    return new_pose
+        goal_orientation_w = '~goal_orientation_w_' + goal_name_identifier
+        new_pose.pose.orientation.w = rospy.get_param(goal_orientation_w)
 
+        return new_pose
 
-rospy.init_node('patroling', anonymous=True)
-counter = 0
-goal_poses_index = 0
-goal_poseA = get_goal_pose_from_cfg('A')
-goal_poseB = get_goal_pose_from_cfg('B')
-goal_poses = [goal_poseA, goal_poseB]
-next_goal_pose = goal_poseA
+    def is_it_near_the_goal(self, robot_x, robot_y, goal_x, goal_y):
+        if robot_x < (goal_x + self.threshold_in_meters) and (robot_x > (goal_x - self.threshold_in_meters)):
+            if robot_y < (goal_y + self.threshold_in_meters) and (robot_y > (goal_y - self.threshold_in_meters)):
+                return True
+        return False
 
+    def set_next_goal_pose(self):
+        self.counter += 1
+        if self.counter > 4:
+            self.counter = 0
+            self.goal_poses_index += 1
+            self.goal_poses_index %= self.number_of_goals
+            self.next_goal_pose = self.goal_poses[self.goal_poses_index]
 
+    def robot_info_callback(self, robot_info):
+        robot_x = robot_info.pose.pose.position.x
+        robot_y = robot_info.pose.pose.position.y
+        goal_x = self.next_goal_pose.pose.position.x
+        goal_y = self.next_goal_pose.pose.position.y
 
-def is_it_near_the_goal(robot_x, robot_y, goal_x, goal_y, threshold_in_meters):
-    if robot_x < (goal_x + threshold_in_meters) and (robot_x > (goal_x - threshold_in_meters)):
-        if robot_y < (goal_y + threshold_in_meters) and (robot_y > (goal_y - threshold_in_meters)):
-            return True
-    return False
+        if self.is_it_near_the_goal(robot_x, robot_y, goal_x, goal_y):
+            self.set_next_goal_pose()
 
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def set_next_goal_pose():
-    global counter, goal_poses_index, next_goal_pose
-    counter += 1
-    if counter > 4:
-        counter = 0
-        goal_poses_index += 1
-        goal_poses_index %= 2
-        next_goal_pose = goal_poses[goal_poses_index]
+    def talker(self):
 
+        while not rospy.is_shutdown():
+            self.next_goal_pose.header.stamp = rospy.get_rostime()
+            self.next_goal_pose.header.frame_id = 'map'
+            self.pub.publish(self.next_goal_pose)
+            self.rate.sleep()
 
-def robot_info_callback(data):
-    robot_info = data
-    robot_x = robot_info.pose.pose.position.x
-    robot_y = robot_info.pose.pose.position.y
-    goal_x = next_goal_pose.pose.position.x
-    goal_y = next_goal_pose.pose.position.y
-
-    if is_it_near_the_goal(robot_x, robot_y, goal_x, goal_y, 0.5):
-        set_next_goal_pose()
-
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-
-def talker():
-    pub = rospy.Publisher('goal', PoseStamped, queue_size=10)
-    rospy.Subscriber("robot_info", RobotInfo, robot_info_callback)
-
-    rate = rospy.Rate(2)  # 10hz
-    while not rospy.is_shutdown():
-        next_goal_pose.header.stamp = rospy.get_rostime()
-        next_goal_pose.header.frame_id = 'map'
-        pub.publish(next_goal_pose)
-        rate.sleep()
+    def get_goal_poses(self):
+        for i in range(65, 65 + self.number_of_goals):
+            goal_pose = self.get_goal_pose_from_cfg(chr(i))
+            self.goal_poses.append(goal_pose)
 
 
 if __name__ == '__main__':
     try:
-        talker()
+        Patroling_node = Patroling()
+        Patroling_node.talker()
     except rospy.ROSInterruptException:
         pass
